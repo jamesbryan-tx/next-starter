b9fc12a760f4e83b4bd1fcece9a36554489ef4f1
jbryan83 <james.bryan.home@gmail.com>
2023-10-04 20:19:59 -0500

    Footer updates

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -8,7 +8,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     <footer className={cn(className)}>
       <div className='container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0'>
         <div className='flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0'>
-          <Icons.logo />
+          <Icons.logo className='h-6 w-6' />
           <p className='text-center text-sm leading-loose md:text-left'>
             Built by{' '}
             <a
@@ -19,14 +19,14 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             >
               Jebulous
             </a>
-            . Hosted on{' '}
+            . Forked from{' '}
             <a
-              href='https://vercel.com'
+              href='https://tx.shadcn.com'
               target='_blank'
               rel='noreferrer'
               className='font-medium underline underline-offset-4'
             >
-              Vercel
+              Taxonomy
             </a>
             . The source code is available on{' '}
             <a
