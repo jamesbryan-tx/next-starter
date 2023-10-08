d52222ab305b6ec4a15cb722bd1738462be8e49b
jbryan83 <james.bryan.home@gmail.com>
2023-10-08 14:54:42 -0500

    Update Twitter icon and add GitHub icon to SiteFooter

--- components/icons.tsx
+++ components/icons.tsx
@@ -37,7 +37,7 @@ export const Icons = {
   spinner: Loader2,
   sun: SunMedium,
   trash: Trash,
-  twitter: Twitter,
+  // twitter: Twitter,
   user: User,
   warning: AlertTriangle,
   logo: (props: LucideProps) => (
@@ -48,7 +48,7 @@ export const Icons = {
       />
     </svg>
   ),
-  gitHub: (props: LucideProps) => (
+  github: (props: LucideProps) => (
     <svg viewBox='0 0 438.549 438.549' {...props}>
       <path
         fill='currentColor'
@@ -62,6 +62,14 @@ export const Icons = {
         fill='currentColor'
         d='M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z'
       ></path>
+    </svg>
+  ),
+  twitter: (props: LucideProps) => (
+    <svg viewBox='0 0 1200 1227' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <path
+        d='M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z'
+        fill='currentColor'
+      />
     </svg>
   ),
 };
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,46 +1,35 @@
+import Link from 'next/link';
+
 import { Icons } from '@/components/icons';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { siteConfig } from '@/config/site';
-import { cn } from '@/lib/utils';
+import { cn, getCurrentYear } from '@/lib/utils';
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
       <div className='container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0'>
-        <div className='flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0'>
-          {/* <Icons.logo className='h-6 w-6' /> */}
-          <p className='text-center text-sm leading-loose md:text-left'>
-            Built by{' '}
-            <a
-              href={siteConfig.links.twitter}
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              Jebulous
-            </a>
-            . Forked from{' '}
-            <a
-              href='https://tx.shadcn.com'
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              Taxonomy
-            </a>
-            . The source code is available on{' '}
-            <a
-              href={siteConfig.links.github}
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              GitHub
-            </a>
-            .
+        <div className='flex flex-col items-center gap-4 px-8 text-center text-sm leading-loose md:flex-row md:gap-2 md:px-0 md:text-left'>
+          <p>
+            &copy; {getCurrentYear()} {siteConfig.name}. All rights reserved.
           </p>
+          <Link href='#' className='font-medium underline underline-offset-4'>
+            Privacy Policy
+          </Link>
+          <p className='hidden md:flex'>{'  |  '}</p>
+          <Link href='#' className='font-medium underline underline-offset-4'>
+            Terms and Conditions
+          </Link>
         </div>
-        <ThemeToggle />
+        <div className='flex items-center gap-4'>
+          <Link href={siteConfig.links.twitter}>
+            <Icons.twitter className='h-5 w-5' />
+          </Link>
+          <Link href={siteConfig.links.github}>
+            <Icons.github className='-mr-2 h-6 w-6' />
+          </Link>
+          <ThemeToggle />
+        </div>
       </div>
     </footer>
   );
