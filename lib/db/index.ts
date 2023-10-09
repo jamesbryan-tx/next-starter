8e60224dd981d3b38226ec8438f961fbc47ac904
jbryan83 <james.bryan.home@gmail.com>
2023-10-09 11:52:59 -0500

    Update database connection to limit maximum number of connections

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,5 +4,5 @@ import postgres from 'postgres';
 import { env } from '@/lib/env.mjs';
 
 const connectionString = env.DATABASE_URL;
-const client = postgres(connectionString);
+const client = postgres(connectionString, { max: 1 });
 export const db = drizzle(client);
